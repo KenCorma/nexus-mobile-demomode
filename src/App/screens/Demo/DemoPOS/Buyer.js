92ca6c15f4a10c4a6b7717a4ca98847b10d057a3
Kendal Cormany <Kendal.Cormany@nexus.io>
2021-03-31 13:21:42 -0700

    buyer listens to new invoices

--- src/App/screens/Demo/DemoPOS/Buyer.js
+++ src/App/screens/Demo/DemoPOS/Buyer.js
@@ -8,16 +8,44 @@ import QRCode from 'react-native-qrcode-svg';
 import Text from 'components/Text';
 import SvgIcon from 'components/SvgIcon';
 import { useTheme, disabledColor } from 'lib/theme';
-import { loadInvoices } from 'lib/demo';
+import { fakeAddInvoice, loadInvoices } from 'lib/demo';
+import { getStore } from 'store';
+
+function watchInvoices(setProcess) {
+  const unsub = getStore().observe(
+    (state) => state,
+    async (state, oldState) => {
+      if (!oldState || !state) return;
+      const txCount = state?.demo?.pos?.invoices;
+      const oldTxCount = oldState?.demo?.pos?.invoices;
+      if (txCount > oldTxCount) {
+        setProcess(2);
+        unsub();
+      }
+    }
+  );
+}
 
 export default function Buyer() {
   const [process, setProcess] = useState(0);
 
   const theme = useTheme();
   const invoices = useSelector((state) => state.demo.pos?.invoices);
+  const latestInvoice =
+    invoices && invoices.length > 0 ? invoices[invoices.length - 1] : '';
   const defaultAddress = useSelector((state) =>
     state.user.accounts.filter((e) => e.name === 'default')
   );
+
+  switch (process) {
+    case 2:
+      console.log(invoices);
+
+      break;
+
+    default:
+      break;
+  }
 
   return (
     <View>
@@ -37,8 +65,10 @@ export default function Buyer() {
               <Button
                 mode="contained"
                 labelStyle={{ fontSize: 12 }}
-                onPress={() => {
-                  loadInvoices();
+                onPress={async () => {
+                  const ssss = await loadInvoices();
+                  console.log(ssss);
+                  watchInvoices(setProcess);
                   setProcess(1);
                 }}
               >
@@ -59,6 +89,44 @@ export default function Buyer() {
                   }
                 />
                 <Text sub>Waiting for response...</Text>
+
+                <Button mode="text" onPress={() => fakeAddInvoice()}>
+                  Add Fake One
+                </Button>
+              </View>
+            </>
+          ),
+          2: (
+            <>
+              <View>
+                <Text>
+                  An Invoice from {latestInvoice.sender} is requesting payment
+                </Text>
+                <Text>Totale {latestInvoice.total}NXS</Text>
+                <Text>This Invoice is for the following items:</Text>
+                {latestInvoice?.items?.map((e) => (
+                  <Text
+                    key={e.units.toString() + e.object + e.unitcost.toString()}
+                  >
+                    {e.units} * {e.object} = {e.units * e.unitcost}
+                  </Text>
+                ))}
+
+                <Button
+                  mode="contained"
+                  onPress={() => {
+                    setProcess(3);
+                  }}
+                >
+                  Pay Now
+                </Button>
+              </View>
+            </>
+          ),
+          3: (
+            <>
+              <View>
+                <Text>Purchase Complete</Text>
               </View>
             </>
           ),
--- src/lib/demo.js
+++ src/lib/demo.js
@@ -5,9 +5,25 @@ import { getStore } from 'store';
 export async function loadInvoices() {
   const store = getStore();
   try {
-    const coreInfo = await callAPI('users/list/invoices');
-    store.dispatch({ type: TYPE.DEMO_LOAD_INVOICES, payload: coreInfo });
+    const invoices = await callAPI('users/list/invoices');
+    store.dispatch({ type: TYPE.DEMO_LOAD_INVOICES, payload: invoices });
+    return invoices;
   } catch (err) {
     throw err;
   }
+}
+
+export function fakeAddInvoice() {
+  const store = getStore();
+  store.dispatch({
+    type: TYPE.DEMO_LOAD_INVOICES,
+    payload: [
+      {
+        name: 'DSsds',
+        total: 10,
+        sender: "Bob's Burger Shop",
+        items: [{ object: 'items1', unitcost: 1, units: 10 }],
+      },
+    ],
+  });
 }
