8c5476b9caefb2b4c5e897aa3f40701244a77816
Kendal Cormany <Kendal.Cormany@nexus.io>
2021-04-08 17:21:42 -0700

    Modified an procedulally generated landscape script to work for react native and to be used for the tokenize demo.

--- src/App/screens/Demo/DemoTokenize/index.js
+++ src/App/screens/Demo/DemoTokenize/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
-import { Surface } from 'react-native-paper';
+import { Button, Surface } from 'react-native-paper';
 
 import Divider from 'components/Divider';
 import InfoField from 'components/InfoField';
 import ScreenBody from 'components/ScreenBody';
 import Text from 'components/Text';
 import { useTheme } from 'lib/theme';
+import { callAPI } from 'lib/api';
+
+import Landscape from './Landscape';
 
 const styles = {
   infoSection: {
@@ -31,6 +34,59 @@ export default function DemoTokenizeScreen() {
 
       <View style={styles.legal}>
         <Text>THIS IS LEFT BLANK</Text>
+
+        <Button
+          onPress={async () => {
+            console.log(
+              'Tokenize Demo'
+            ); /*
+            const params = {
+              pin: '1234',
+              address: '878b6SR2AA2N6eZCEGq7N5itKckviMvSPdpis4XMX54q2bxRD2G',
+              token_name: 'phone:ABC',
+            };
+
+            const params2 = {
+              pin: '1234',
+              name: 'ABC',
+              supply: 1000,
+              decimals: 2,
+            };
+            const result = await callAPI('tokens/create/token', params2);
+            console.log(result);
+            const tokenresult = await callAPI('assets/tokenize/asset', params);
+            console.log(tokenresult);
+            const list = await callAPI('users/list/tokens');
+            const assets = await callAPI('users/list/assets');
+            console.log(list);
+            console.log(assets);
+
+            const makeaccparams = {
+              pin: '1234',
+              token_name: 'phone:ABC',
+              name: 'testABC',
+            };
+            const makeaccount = await callAPI(
+              'tokens/create/account',
+              makeaccparams
+            );
+            console.log(makeaccount);
+
+            const creditparams = {
+              pin: '1234',
+              name: 'ABC',
+              amount: 500,
+              name_to: 'testABC',
+            };
+            const credit = await callAPI('tokens/debit/token', creditparams);
+            console.log(credit);
+            */
+
+            console.log(await callAPI('users/list/tokens'));
+          }}
+        >
+          test
+        </Button>
       </View>
     </ScreenBody>
   );
